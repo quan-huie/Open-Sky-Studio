c8ac05dca041165af5d91d4af3f0c4b2f8bb2ba2
Quan Huie <quanhuie@gmail.com>
2020-08-26 20:26:14 -0700

    added updated files

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+      <div className="HomePage">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
