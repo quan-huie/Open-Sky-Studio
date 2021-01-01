dc36e11336126e5b6330003ac43f3994907d176e
Quan Huie <quanhuie@gmail.com>
2021-01-01 15:04:43 -0800

    added comments

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './HomePage.css';
 
+// Bootstrap Components
+import {Jumbotron} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+
 function HomePage() {
     return (
       <div className="HomePage">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <Jumbotron>
+            
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+            </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+        </Jumbotron>
       </div>
     );
   }
