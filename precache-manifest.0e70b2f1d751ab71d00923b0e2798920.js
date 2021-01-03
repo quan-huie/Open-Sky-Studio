18192a3ef69fc2be6acd112e469f223d6d1954b1
Quan Huie <quanhuie@gmail.com>
2021-01-03 13:34:14 -0800

    Updates

---
+++ precache-manifest.0e70b2f1d751ab71d00923b0e2798920.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42215481da1486cca4f05dd64940d271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42cf6deab1a2b7866905",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5ccbc085402128ffcce0",
+    "url": "/static/css/main.0cfa835c.chunk.css"
+  },
+  {
+    "revision": "42cf6deab1a2b7866905",
+    "url": "/static/js/2.17bab290.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.17bab290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccbc085402128ffcce0",
+    "url": "/static/js/main.ff004ee5.chunk.js"
+  },
+  {
+    "revision": "c2af49b922e7e0f83c38",
+    "url": "/static/js/runtime-main.b38caf4a.js"
+  },
+  {
+    "revision": "e730c46379b0d3fcd6f8c6b5e9e75248",
+    "url": "/static/media/logo.e730c463.png"
+  },
+  {
+    "revision": "bf9948a2a253c0990befd52f5f1c7e7a",
+    "url": "/static/media/massage.bf9948a2.jpg"
+  },
+  {
+    "revision": "ea43db3e9432f0a96f1f5bb686e02867",
+    "url": "/static/media/open-sky-banner.ea43db3e.jpg"
+  },
+  {
+    "revision": "1f34c27ffad842141633af06198d87ba",
+    "url": "/static/media/pilates.1f34c27f.jpg"
+  },
+  {
+    "revision": "3a3836b9097a944b6597064170eec8e5",
+    "url": "/static/media/stretching.3a3836b9.jpg"
+  },
+  {
+    "revision": "374300dfa435bf3fa8b2facda6b238f9",
+    "url": "/static/media/studio-img-1.374300df.jpg"
+  },
+  {
+    "revision": "5d7afc780b6fad22dfaa4f90fe6263d3",
+    "url": "/static/media/studio-img-2.5d7afc78.jpg"
+  },
+  {
+    "revision": "c631ea336c43176062c9690c9cb4a68b",
+    "url": "/static/media/studio-img-3.c631ea33.jpg"
+  }
+]);
