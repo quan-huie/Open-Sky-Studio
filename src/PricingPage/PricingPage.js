79210291e7597d45200eb409534a77b18a6a60c1
Jaden Heller <jaheller@chapman.edu>
2025-03-12 17:42:51 -0700

    Update PricingPage.js

--- src/PricingPage/PricingPage.js
+++ src/PricingPage/PricingPage.js
@@ -13,18 +13,17 @@ function PricingPage() {
           </h4>
           <br/>
           <h5>50 minutes</h5>
-
-          <p>Private session (50 min) $80
+            <p>Private session (50 min) $80
             <br/>Duet session (50 min) $55
             <br/>Trio session (50 min) $45
             <br/>Quad session (50 min) $35
           </p>
 
           <h5>80 minutes</h5>
-          <p>Private session (80 min) $90
-            <br/>Duet session (80 min) $75
-            <br/>Trio session (80 min) $65
-            <br/>Quad session (80 min) $55</p>
+            <p>Private session (80 min) $130
+            <br/>Duet session (80 min) $80
+            <br/>Trio session (80 min) $70
+            <br/>Quad session (80 min) $60</p>
 
         </div>
 
@@ -37,17 +36,17 @@ function PricingPage() {
         <div className = "pricing-section">
           <h3>MEDITATION (ONLY)</h3>
           <h5>30 minutes</h5>
-          <p>Private session (30 min) $65
-            <br/>Duet session (30 min) $35
+            <p>Private session (30 min) $35
+            <br/>Duet session (30 min) $25
             <br/>Trio session (30 min) $20
             <br/>Quad session (30 min) $15
           </p>
 
           <h5>50 minutes</h5>
-          <p>Private session (50 min) $80
-            <br/>Duet session (50 min) $55
+            <p>Private session (50 min) $55
+            <br/>Duet session (50 min) $45
             <br/>Trio session (50 min) $35
-            <br/>Quad session (50 min) $25
+            <br/>Quad session (50 min) $30
           </p>
         </div>
       </div>
